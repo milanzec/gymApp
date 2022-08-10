870a9fce4f81100f7c52c3dda120ba7700166d5e
unknown <milanzec53@gmail.com>
2022-08-10 17:23:47 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Route,Router} from 'react-router-dom'
 import './App.css'
+
+import Navbar from './components/Navbar'
+import {Box} from '@mui/material'
 
 const App = () => {
   return (
     <div>
-        <h1>Gym App</h1>
+        <Box  width="400px"  >
+            <Navbar /> 
+            <Router>
+                <Route/>
+                <Route/>
+            </Router>
+        </Box>
     </div>
   )
 }
---
+++ src/components/pages/ExersizeDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExersizeDetail = () => {
+  return (
+    <div>ExersizeDetail</div>
+  )
+}
+
+export default ExersizeDetail
