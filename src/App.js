f2bc7c7b845dfae3b3ccd63c13fe7b10597552ac
unknown <milanzec53@gmail.com>
2022-08-11 14:16:01 +0200

    browserRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import {Route,Router} from 'react-router-dom'
+import {Route,Routes} from 'react-router-dom'
 import './App.css'
 
+import ExersizeDetail from './components/pages/ExersizeDetail'
+import Home from './components/pages/Home'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import {Box} from '@mui/material'
 
 const App = () => {
   return (
     <div>
-        <Box  width="400px"  >
+        <Box  width="400px" sx={{width:{xl:'1448px'}}} m="auto">
             <Navbar /> 
-            <Router>
-                <Route/>
-                <Route/>
-            </Router>
+            <Routes>
+                <Route path='/'  element={<Home />}/>
+                <Route path='/exersize/:id'  element={<ExersizeDetail />}/>
+            </Routes>
         </Box>
+        <div>
+          <Footer />
+        </div>
     </div>
   )
 }
