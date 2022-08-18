9dbfbe3fcf66f5d914a24181f503921a7aeb84a3
unknown <milanzec53@gmail.com>
2022-08-18 17:17:43 +0200

    homepagebtn

--- src/components/pages/HeroBanner.js
+++ src/components/pages/HeroBanner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box,Stack,Typography} from '@mui/material'
+import {Box,Stack,Typography,Button} from '@mui/material'
 
 import HomePic from '../../assets/images/banner.png'
 
@@ -9,7 +9,7 @@ const HeroBanner = () => {
     <Box
     sx={{
         mt:{lg:'212px',xs:'70px'},
-        ml:{sm:'50px'},position:'relative'
+        ml:{sm:'50px'},position:'relative',p:"26px"
     }}
     >
 <Typography
@@ -20,9 +20,21 @@ color="#FF2625"
 >
     Neki natpis
 </Typography>
+<Typography
+fontWeight={700}
+sx={{
+    fontSize:{lg:'44px',xs:'39px'}
+}}
+>EAT <br/> SLEEP <br/> TRAIN <br/> REPEAT
+
+</Typography>
+<Typography fontSize="25px" lineHeight="35px" marginBottom={4}>
+    All the best exersizes at one place
+</Typography>
 
         <img src={HomePic} className ="hero-banner-img"/>
-    </Box>
+    </Box> 
+    <Button variant='outlined' sx={{color:'#FF2625'}}>Find exercise</Button>
     </div>
   )
 }
