d47466c2952955e7f8ed01485355006bab1cadcd
unknown <milanzec53@gmail.com>
2023-04-21 17:51:37 +0200

    gifUrl detail page

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Stack, Button, Typography } from "@mui/material";
 
-const Detail = () => {
+const Detail = ({ exerciseDetail }) => {
+  const { name, gifUrl, bodyPart, equipment, target } = exerciseDetail;
+
   return (
-    <div>Detail</div>
-  )
-}
+    <Stack
+      sx={{
+        gap: "60px",
+        alignItems: "center",
+        p: "20px",
+        flexDirection: { lg: "row" },
+      }}
+    >
+      <img src={gifUrl} alt={name} className="detail-image" loading="lazy" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography>{name}</Typography>
+        <Typography>
+          Exercises keep you strong.{name} {` `}
+          is one of the best exercises to target your {target}.It will help you
+          improve your mood and gain energy.
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
 
-export default Detail
+export default Detail;
--- src/components/pages/ExersizeDetail.js
+++ src/components/pages/ExersizeDetail.js
@@ -6,12 +6,30 @@ import Detail from "../Detail";
 import ExersizeVideos from "../ExersizeVideos";
 import SimilarExercises from "../SimilarExercises";
 
-import { exerciseOptions, fetchData } from "../../utils/fetchData";
+import { exersizeOptions, fetchData } from "../../utils/fetchData";
 
 const ExersizeDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+
+      const exerciseData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exersizeOptions
+      );
+      setExerciseDetail(exerciseData);
+    };
+
+    fetchExerciseData();
+    console.log(exerciseDetail);
+  }, [id]);
+
   return (
     <Box>
-      <Detail />
+      <Detail exerciseDetail={exerciseDetail} />
       <ExersizeVideos />
       <SimilarExercises />
     </Box>
