777f6be5e6f1cfae78c0a788441a652f42ae7201
unknown <milanzec53@gmail.com>
2023-04-26 16:03:44 +0200

    Detail done

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { Stack, Button, Typography } from "@mui/material";
 
+import BodypartImage from "../assets/icons/body-part.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+import TargetImage from "../assets/icons/target.png";
+
 const Detail = ({ exerciseDetail }) => {
   const { name, gifUrl, bodyPart, equipment, target } = exerciseDetail;
+
+  const extraDetail = [
+    {
+      icon: BodypartImage,
+      name: bodyPart,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+  ];
 
   return (
     <Stack
@@ -15,12 +34,33 @@ const Detail = ({ exerciseDetail }) => {
     >
       <img src={gifUrl} alt={name} className="detail-image" loading="lazy" />
       <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
-        <Typography>{name}</Typography>
-        <Typography>
-          Exercises keep you strong.{name} {` `}
+        <Typography variant="h3">{name}</Typography>
+        <Typography variant="h5">
+          Exercises keep you strong. {name} {` `}
           is one of the best exercises to target your {target}.It will help you
           improve your mood and gain energy.
         </Typography>
+        {extraDetail.map((item) => (
+          <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+            <Button
+              sx={{
+                background: "#fff2db",
+                width: "100px",
+                height: "100px",
+                borderRadius: "50%",
+              }}
+            >
+              <img
+                src={item.icon}
+                alt={bodyPart}
+                style={{ width: "50px", height: "50px" }}
+              />
+            </Button>
+            <Typography variant="h5" textTransform="capitalize">
+              {item.name}
+            </Typography>
+          </Stack>
+        ))}
       </Stack>
     </Stack>
   );
