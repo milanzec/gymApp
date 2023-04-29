21d7c607492eb7d5c6b9ab8d65ee9a4eafc3fb22
unknown <milanzec53@gmail.com>
2023-04-29 14:19:14 +0200

    Footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box mt="80px" bgcolor="#d7d7c1">
+      <Stack gap="40px" alignItems="center" pb="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" pb="40px" mt="20px" textTransform="capitalize">
+          made by milan zec
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
