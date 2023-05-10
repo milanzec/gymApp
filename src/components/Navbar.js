42e9533cc5ec2c3c72c1c084281db26b8a3c93a2
unknown <milanzec53@gmail.com>
2023-05-10 13:52:14 +0200

    navber and button href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import Home from './pages/Home'
-import {Link}  from 'react-router-dom'
-import {Stack, Typography} from '@mui/material'
+import React from "react";
+import Home from "./pages/Home";
+import { Link } from "react-router-dom";
+import { Stack, Typography } from "@mui/material";
 
-import Logo from '../assets/images/Logo.png'
+import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
   return (
-  <Stack
-  direction="row"
-  justifyContent="space-around"
-  sx={{gap:{sm:"120px",xs:"40px"},mt:{sm:'32px',xs:'20px'},justifyContent:'none',px:"20px"}}
-  >
-    <Link to="/">
-    <img src={Logo}  alt="logo" style={{width:'45px',height:'45px',margin:'5px 20px'}}/>
-    </Link>
-    <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
-      <Link to={Home} style={{textDecoration:'none',color:"#3A1212",borderBottom:"3px solid #FF2625"}}>Home</Link>
-      <a href='#exersizes' style={{textDecoration:'none',color:"#3A1212",borderBottom:"3px solid #FF2625"}}>Exersizes</a>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: "120px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+        px: "20px",
+      }}
+    >
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "45px", height: "45px", margin: "5px 20px" }}
+        />
+      </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <Link
+          to={Home}
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+            borderBottom: "3px solid #FF2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+            borderBottom: "3px solid #FF2625",
+          }}
+        >
+          Exercises
+        </a>
+      </Stack>
     </Stack>
-  </Stack>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/pages/HeroBanner.js
+++ src/components/pages/HeroBanner.js
@@ -1,43 +1,55 @@
-import React from 'react'
-import {Box,Stack,Typography,Button} from '@mui/material'
+import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
 
-import HomePic from '../../assets/images/gym andrea.jpg'
+import HomePic from "../../assets/images/gym andrea.jpg";
 
 const HeroBanner = () => {
   return (
-
     <Box
-    sx={{
-        mt:{lg:'212px',xs:'70px'},
-        ml:{sm:'50px'},position:'relative',p:"26px"
-    }}
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+        position: "relative",
+        p: "26px",
+      }}
     >
-<Typography
-fontWeight="600"
-fontSize="25px"
-color="#FF2625"
+      <Typography fontWeight="600" fontSize="25px" color="#FF2625">
+        Neki natpis
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44px", xs: "39px" },
+        }}
+      >
+        EAT <br /> SLEEP <br /> TRAIN <br /> REPEAT
+      </Typography>
+      <Typography fontSize="25px" lineHeight="35px" marginBottom={4}>
+        All the best exersizes at one place
+      </Typography>
 
->
-    Neki natpis
-</Typography>
-<Typography
-fontWeight={700}
-sx={{
-    fontSize:{lg:'44px',xs:'39px'}
-}}
->EAT <br/> SLEEP <br/> TRAIN <br/> REPEAT
+      <img src={HomePic} className="hero-banner-img" />
+      <Stack>
+        <a
+          href="#exercises"
+          style={{
+            marginTop: "45px",
+            textDecoration: "none",
+            width: "200px",
+            textAlign: "center",
+            background: "#FF2625",
+            padding: "14px",
+            fontSize: "18px",
+            textTransform: "capitalize",
+            color: "white",
+            borderRadius: "4px",
+          }}
+        >
+          find exercises
+        </a>
+      </Stack>
+    </Box>
+  );
+};
 
-</Typography>
-<Typography fontSize="25px" lineHeight="35px" marginBottom={4}>
-    All the best exersizes at one place
-</Typography>
-
-        <img src={HomePic} className ="hero-banner-img"/>
-        <Button variant='outlined' sx={{color:'#FF2625'}} >Find exercise</Button>
-    </Box> 
-    
-
-  )
-}
-
-export default HeroBanner
+export default HeroBanner;
